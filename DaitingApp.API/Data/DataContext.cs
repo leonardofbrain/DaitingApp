180b7d928fad875b4e9e3fd1f537c3724d4b25ac
Leonardo <leonardofbrain@gmail.com>
2019-12-22 19:10:41 -0300

    Initial Commit

---
+++ DaitingApp.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using DaitingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DaitingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
